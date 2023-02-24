6ad326850fd8d23d660598caa230b95a9c9634ce
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-02-24 21:19:57 +0300

    fix responsive design issues on recommendations

--- app/components/Music.tsx
+++ app/components/Music.tsx
@@ -4,29 +4,16 @@ export default function Music({
   publishedDate,
   trackName,
   trackLink,
-  icon,
 }: MusicInterface) {
   return (
     <ul className="py-2">
       <li className="flex items-center">
-        <div className="flex">
+        <div className="flex justify-between">
           <p className="text-sm text-gray-400 mr-2 w-24">{publishedDate}</p>
           <a href={trackLink} target="_blank" className="mx-auto">
-            <p className="italic underline">{trackName}</p>
+            <p className="italic w-48 md:w-full underline">{trackName}</p>
           </a>
         </div>
-
-        {/* <div className="flex items-center gap-4">
-          <a href={trackLink} target="_blank">
-            <img
-              className="rounded-full"
-              width={32}
-              height={32}
-              src={`icons/${icon}.png`}
-              alt="soundcloud"
-            />
-          </a>
-        </div> */}
       </li>
     </ul>
   );
--- app/components/Recommendation.tsx
+++ app/components/Recommendation.tsx
@@ -7,22 +7,19 @@ export default function Recommendation() {
       <p className="mb-4 text-gray-400">Recommendations</p>
       <h3 className="text-lg font-bold mb-2">Music 🎵</h3>
       <Music
-        publishedDate="5 Mar. 2016"
+        publishedDate="05 Mar. 2016"
         trackName="Fordham Spring Weekend Audition 2"
         trackLink="https://soundcloud.com/ya-zhan-necat-yakali/sets/recommendations?si=549674ab17c246c69d35173d5a9f1505&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-        icon="soundcloud"
       ></Music>
       <Music
-        publishedDate="24 April 2012"
+        publishedDate="24 Apr. 2012"
         trackName="The Hobbit - Over The Misty Mountains Cold"
         trackLink="https://soundcloud.com/kieren-leonardo-hovasapian/the-hobbit-over-the-misty?in=ya-zhan-necat-yakali/sets/recommendations&si=ef9efb643e574cfe8545bb718278d321&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-        icon="soundcloud"
       ></Music>
       <Music
         publishedDate="01 Dec. 2022"
         trackName="Shaun Farrugia - Dear God"
         trackLink="https://music.youtube.com/watch?v=s3FAw63hmQ8&feature=share"
-        icon="youtube"
       ></Music>
 
       <h3 className="text-lg font-bold my-2">Podcast 🎙</h3>
@@ -30,7 +27,6 @@ export default function Recommendation() {
       <Podcast
         podcastName="Barış Özcan ile 111 Hz (for my Turkish friends 😉)"
         podcastLink="https://open.spotify.com/show/5NbxzMRbuun0SIOh7GKMbk?si=a7439d19603a4c9f"
-        icon="spotify"
       ></Podcast>
 
       <hr className="my-5" />
