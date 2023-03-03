b4c6df4eca704f47e9cf317c6c8d3f0df2317428
Yakali, Yagizhan Necat (ADV D EU TR AP&I PIA1) <yagizhan.yakali@siemens.com>
2023-03-03 22:30:12 +0300

    add nuget library

--- app/components/Libraries.tsx
+++ app/components/Libraries.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Libraries() {
   return (
     <div className="flex flex-col w-full justify-center max-w-xl">
       <p className="mb-4 text-gray-400">Published Libraries</p>
-      <h3 className="text-lg font-bold mb-2">NPM</h3>
+      <h3 className="text-lg font-bold my-2">NPM</h3>
       <Link
         className="italic w-48 md:w-full underline"
         href={"https://www.npmjs.com/package/node-network-scan"}
@@ -18,6 +17,23 @@ export default function Libraries() {
         <Link
           className="underline italic"
           href={"https://github.com/yagizhanNY/node-network-scanner"}
+        >
+          link.
+        </Link>
+      </p>
+      <h3 className="text-lg font-bold my-2">Nuget</h3>
+      <Link
+        className="italic w-48 md:w-full underline"
+        href={"https://www.nuget.org/packages/NetworkScanner/"}
+      >
+        NetworkScanner
+      </Link>
+      <p className="text-sm mt-1 text-gray-400 italic">
+        Light weight library for scan the local network fastly. To contribute
+        this library please follow the{" "}
+        <Link
+          className="underline italic"
+          href={"https://github.com/yagizhanNY/net-network-scanner"}
         >
           link.
         </Link>
